a5539a266d872c1d772d7325984c674fb74c574a
Benjamin Michael Comer <bcomer3@login-s2.pace.gatech.edu>
2019-05-07 15:28:04 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
-import sys
-import os
-script_path = os.path.dirname(os.path.abspath( __file__ ))
+#!/usr/bin/env python
 
+from distutils.core import setup
+from setuptools import find_packages
 
-
-print('This is not a sophisticated installation file. To install this python package you should add the\ndirectory this script is in to your PYTHONPATH environment varible. here\'s a suggested line to add\nto your ~/.bashrc file:')
-print('export PYTHONPATH=' + script_path + ':$PYTHONPATH')
-
-"""
-from setuptools import setup
-
-setup(name='pysparcx',
-          version='0.1',
-          description='The python wrapper for the ab inito code SPARC-X',
-          url='https://github.com/SPARC-X/ase-sparc.git',
-          author='Flying Circus',
-          author_email='',
-          license='MIT',
-          install_requires=[
-          'ase',
-          'numpy',
-          'scipy'],
-          packages=['pysparcx'],
-          zip_safe=False)
-"""
+setup(name='pysparc_x',
+      version='0.1',
+      description='Python Wrapper for the SPARC-X DFT Code',
+      author='Ben Comer',
+      author_email='ben.comer@gatech.edu',
+      url='https://github.com/SPARC-X/pysparcx/tree/new_input',
+      packages=find_packages(),
+      install_requires=['spglib', 'numpy','ase','scipy'],
+     )
