527c6c713f66d9dd7e1ad548b2cb27868efc88df
Benjamin Comer <benjamincomer@ipsec-172-16-85-41.vpn.gatech.edu>
2020-07-15 14:52:02 -0400

    removed python 3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(name='sparc-dft-api',
       version='0.1',
-      python_requires='>3.4',
+      python_requires='>3.5',
       description='Python Wrapper for the SPARC-X DFT Code',
       author='Ben Comer',
       author_email='ben.comer@gatech.edu',
