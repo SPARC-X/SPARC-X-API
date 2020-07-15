554117398815f6fd7a434a61a212e9692680fd59
Benjamin Comer <benjamincomer@ipsec-172-16-85-41.vpn.gatech.edu>
2020-07-15 14:36:14 -0400

    added python version requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='sparc-dft-api',
-      version='0.2',
+      version='0.1',
+      python_requires='>3.6',
       description='Python Wrapper for the SPARC-X DFT Code',
       author='Ben Comer',
       author_email='ben.comer@gatech.edu',
