c1302b4e908f67597dd047c03ea991f430d5aed6
T.Tian <alchem0x2a@gmail.com>
2023-04-13 10:43:59 -0400

    unit tests for api validation & conversion

--- tests/test_sparc_input_api.py
+++ tests/test_sparc_input_api.py
@@ -1,8 +1,10 @@
 import pytest
 
+
 def test_sparc_api():
     from sparc.inputs import SparcInputs
     from sparc.inputs import default_json_api
+
     # the default api should always exist
     assert default_json_api.is_file()
     sis = SparcInputs()
@@ -12,15 +14,20 @@ def test_sparc_api():
     assert isinstance(sis.parameters, dict)
     assert hasattr(sis, "other_parameters")
 
+
 def test_api_validate():
     from sparc.inputs import SparcInputs
     import numpy as np
+
     sis = SparcInputs()
     # Integer
     assert sis.validate_input("CALC_PRES", "0")
     assert sis.validate_input("CALC_PRES", "0 ")
     assert sis.validate_input("CALC_PRES", 0)
     assert sis.validate_input("CALC_PRES", True)
+    # Invalid parameters
+    with pytest.raises(KeyError):
+        sis.validate_input("CALC_PRESSURE", True)
     # Double
     assert sis.validate_input("ELEC_TEMP", "200")
     assert sis.validate_input("ELEC_TEMP", "200.0")
@@ -34,17 +41,45 @@ def test_api_validate():
     # TODO: exceptions for cases where int cannot be presented as bool!
     # Double array
     assert sis.validate_input("LATVEC", "1.0 0 0\n0 1.0 0\n0 0 1.0")
-    assert sis.validate_input("LATVEC", [[1.0, 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+    assert sis.validate_input(
+        "LATVEC", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    )
     assert sis.validate_input("LATVEC", np.eye(3))
     # TODO: extra user cases for character !
 
+
 def test_api_validate_all_defaults():
-    """All defaults given in the examples should be valid!
-    """
+    """All defaults given in the examples should be valid!"""
     from sparc.inputs import SparcInputs
     import numpy as np
+
     sis = SparcInputs()
     for param, pd in sis.parameters.items():
         default = pd.get("default", None)
         if default and (default != "auto"):
             assert sis.validate_input(param, default)
+
+
+def test_api_convert_string():
+    """Test if read string makes sense"""
+    from sparc.inputs import SparcInputs
+    import numpy as np
+
+    sis = SparcInputs()
+    # Integer
+    assert sis.convert_string_to_value("CALC_PRES", "0") is False
+    assert sis.convert_string_to_value("CALC_PRES", "0 ") == 0
+    assert sis.convert_string_to_value("CALC_PRES", "1") is True
+    # Double
+    assert sis.convert_string_to_value("ELEC_TEMP", "200") == 200
+    assert sis.convert_string_to_value("ELEC_TEMP", "200.0") == 200
+    # Integer array
+    relax_arr = sis.convert_string_to_value("RELAX", "0 0 0")
+    print("relax,", relax_arr)
+    assert len(relax_arr) == 3
+    assert all([not p for p in relax_arr])
+    # TODO: exceptions for cases where int cannot be presented as bool!
+    # Double array
+    latvec = sis.convert_string_to_value("LATVEC", "1.0 0 0\n0 1.0 0\n0 0 1.0")
+    print("LATVEC", latvec)
+    assert np.isclose(latvec, np.eye(3)).all()
