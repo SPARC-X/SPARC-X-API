8aa806839224fd366ea4a7ddfd65a8e787e73ced
T.Tian <alchem0x2a@gmail.com>
2023-04-13 12:57:54 -0400

    add a sample ion file

--- sparc/sparc_parsers/utils.py
+++ sparc/sparc_parsers/utils.py
@@ -1,3 +1,5 @@
+from warnings import warn
+
 def get_label(fileobj, ext):
     """Return the label of file by stripping the extension (e.g. .ion)"""
     return fileobj.name.rsplit(ext, 1)[0]
@@ -23,3 +25,36 @@ def bisect_and_strip(text, delimiter):
     # wrap around to len(text) if not found (-1)
     index = text.find(delimiter) % (len(text) + 1)
     return text[:index].strip(), text[index + len(delimiter) :].strip()
+
+def read_block_input(block, validator=None):
+    """Read blocks of inputs from ion or inpt file and convert with validator
+    """
+    block_dict = {}
+    multiline_key = ""
+    use_validator = True if validator else False
+    for line in block:
+        if ":" not in line:
+            # no key, assume multiline value
+            block_dict[multiline_key].append(line.strip())
+            continue
+        key, value = bisect_and_strip(line, ":")
+        key = key.upper()
+        # print(key, value)
+        if key and value:
+            block_dict[key] = value
+        elif key:
+            # no value, assume that this key has a list of values
+            # in the following lines
+            block_dict[key] = []
+            multiline_key = key
+    for key, val in block_dict.items():
+        # print(key, val)
+        _use_validator_this_key = use_validator
+        if _use_validator_this_key:
+            if key not in validator.parameters.keys():
+                warn(f"Key {key} not in validator's parameter list, ignore value conversion!")
+                _use_validator_this_key = False
+        if _use_validator_this_key:
+            val = validator.convert_string_to_value(key, val)
+        block_dict[key] = val
+    return block_dict
