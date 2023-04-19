e72867f4c15717da68ba4f80f1abcaf8c7a70451
T.Tian <alchem0x2a@gmail.com>
2023-04-19 14:34:09 -0400

    add test variant in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+test_requires = [
+    "pytest",
+    "pyfakefs",
+    "pytest-cov"
+]
+
 setup(
     name="sparc-dft-api",
     version="0.1",
@@ -12,10 +18,16 @@ setup(
     author_email="ben.comer@gatech.edu",
     url="https://github.com/SPARC-X/sparc-dft-api",
     packages=find_packages(),
-    install_requires=["spglib", "numpy", "ase", "scipy"],
+    install_requires=["spglib",
+                      "numpy>=1.20",
+                      "ase>=3.22.0",
+                      "scipy"],
     entry_points={
         "ase.io": [
             "sparc = sparc.sparc_io_bundle",
         ],
+    },
+    extras_require={
+        'test': test_requires,
     },
 )
