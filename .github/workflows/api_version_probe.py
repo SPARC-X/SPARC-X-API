670ab8bdf2c9f6ed254ffdd5af5938eb9eed5e68
T.Tian <alchem0x2a@gmail.com>
2024-01-16 11:36:01 +0800

    add changes to workflow

---
+++ .github/workflows/api_version_probe.py
@@ -0,0 +1,22 @@
+import sys
+
+from packaging import version
+
+from sparc.api import SparcAPI  # Replace with your actual module import
+
+
+def compare_versions():
+    default_version = SparcAPI().sparc_version
+    new_version = SparcAPI(json_api="parameters.json").sparc_version
+    print(f"Default api version: {default_version}, upstream version {new_version}")
+
+    if version.parse(default_version) < version.parse(new_version):
+        print(f"Version changed from {default_version} to {new_version}")
+        sys.exit(1)  # Exit with a non-zero status to indicate change
+    else:
+        print("No version change")
+        sys.exit(0)  # Exit with zero status to indicate no change
+
+
+if __name__ == "__main__":
+    compare_versions()
