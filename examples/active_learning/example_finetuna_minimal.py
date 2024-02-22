a1627238c6c8115c74165f13ae4052e88d054716
T.Tian <alchem0x2a@gmail.com>
2024-02-22 17:04:07 +0000

    add simple example with socket + finetuna

--- examples/active_learning/example_finetuna_minimal.py
+++ examples/active_learning/example_finetuna_minimal.py
@@ -11,7 +11,10 @@ import yaml
 from pathlib import Path
 from ase.io.trajectory import Trajectory
 from ase.optimize import BFGS
+from ase.constraints import FixAtoms
 from finetuna.ml_potentials.finetuner_ensemble_calc import FinetunerEnsembleCalc
+import ase
+from ase.cluster.cubic import FaceCenteredCubic
 from finetuna.online_learner.online_learner import OnlineLearner
 import argparse
 from sparc.calculator import SPARC
@@ -43,12 +46,39 @@ ml_potential = FinetunerEnsembleCalc(
 )
 
 
-init_molecule = molecule("H2O", pbc=False, cell=[8, 8, 8])
+# init_molecule = molecule("H2O", pbc=False, cell=[8, 8, 8])
+# init_molecule.center()
+# init_molecule.rattle()
 
-sparc_params = {"xc": "pbe", "h": 0.22}
-with SPARC(**sparc_params) as parent_calc:
+
+surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
+layers = [1, 2, 1]
+lc = 3.61000
+
+# init_atoms = molecule("CH4", pbc=True, cell=[8, 8, 8])
+# init_atoms.constraints = [FixAtoms([0])]
+# init_atoms.center()
+# init_atoms.rattle(0.05)
+
+init_atoms = FaceCenteredCubic('Cu', surfaces, layers, latticeconstant=lc)
+init_atoms.cell = [12, 12, 12]
+init_atoms.center()
+init_atoms.pbc = False
+init_atoms.rattle(0.05)
+
+
+sparc_params = {"xc": "pbe", "h": 0.13}
+# with SPARC(directory="pure_BFGS", **sparc_params) as calc:
+#     atoms = init_atoms.copy()
+#     atoms.calc = calc
+#     dyn = BFGS(atoms, maxstep=0.2, trajectory="pure_bfgs.traj")
+#     dyn.run(fmax=0.03)
+    
+
+with SPARC(directory="online_coldstart", **sparc_params) as parent_calc:
+    atoms = init_atoms.copy()
     onlinecalc = OnlineLearner(learner, [], ml_potential, parent_calc)
-    init_molecule.calc = onlinecalc
-    dyn = BFGS(init_molecule,
+    atoms.calc = onlinecalc
+    dyn = BFGS(atoms,
                maxstep=0.2)
     dyn.run(fmax=0.03)
